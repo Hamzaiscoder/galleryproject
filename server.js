76d863254be2afc727fb3aeb3090178e4a696bc7
Hamzaiscoder <hamzadreamhb@gmail.com>
2024-06-24 11:18:42 +0500

    forEach method use

--- server.js
+++ server.js
@@ -1,71 +1,21 @@
-let flower = document.getElementById('flower');
-let earth = document.getElementById('earth');
-let forest = document.getElementById('forest');
-let lake = document.getElementById('lake');
-let moon = document.getElementById('moon');
-let sky = document.getElementById('sky');
+let elements=[
+    document.getElementById('flower'),
+    document.getElementById('forest'),
+    document.getElementById('sky'),
+    document.getElementById('earth'),
+    document.getElementById('moon'),
+    document.getElementById('lake')
+];
 let input = document.getElementById('input');
+
 function search(){
-
-if(input.value.toLowerCase() == "earth")
-{
-    earth.style.display= "block";
-    flower.style.display="none"
-    forest.style.display="none"
-    lake.style.display="none"
-    moon.style.display= "none";
-    sky.style.display= "none";
-}
-
-if(input.value.toLowerCase() == "flower")
-{
-    flower.style.display= "block";
-    earth.style.display="none"
-    forest.style.display="none"
-    lake.style.display="none"
-    moon.style.display= "none";
-    sky.style.display= "none";
-
-}
-
-if(input.value.toLowerCase() == "forest")
-{
-    forest.style.display= "block";
-    flower.style.display="none"
-    earth.style.display="none"
-    lake.style.display="none"
-    moon.style.display= "none";
-    sky.style.display= "none";
-}
-
-if(input.value.toLowerCase() == "lake")
-{
-    lake.style.display= "block";
-    flower.style.display="none"
-    forest.style.display="none"
-    earth.style.display="none"
-    moon.style.display= "none";
-    sky.style.display= "none";
-}
-if(input.value.toLowerCase() == "moon")
-{
-    moon.style.display= "block";
-    flower.style.display="none"
-    forest.style.display="none"
-    earth.style.display="none"
-    lake.style.display="none"
-    sky.style.display= "none";
-    
-}
-if(input.value.toLowerCase() == "sky")
-    {
-        sky.style.display= "block";
-        moon.style.display= "none";
-        flower.style.display="none"
-        forest.style.display="none"
-        earth.style.display="none"
-        lake.style.display="none"
-    
-        
+let value = input.value.toLowerCase();
+elements.forEach(element => {
+    if(element.id === value){
+        element.style.display ="block"
     }
+    else{
+        element.style.display ="none"
+    }
+})
 }
